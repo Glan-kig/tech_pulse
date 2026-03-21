52851c77b83b7e927b42ada4f5b87705c2657090
Vlan <23ik069si@esisalama.org>
2026-03-21 10:08:49 +0100

    add template home.html and show articles

--- core/news/views.py
+++ core/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def home(request):
+    # On recupère tous les articles, triés par date de création décroissante, et on précharge les sources associées pour éviter les requêtes supplémentaires
+    articles = Article.objects.all().order_by('-created_at').prefetch_related('sources')
+    return render(request, 'news/home.html', {'articles': articles})
