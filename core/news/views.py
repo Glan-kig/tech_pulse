f6a1b97948ed76eee9ef55e886e4dae3b6514ff9
Vlan <23ik069si@esisalama.org>
2026-03-28 00:14:37 +0100

    update home.html

--- core/news/views.py
+++ core/news/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Article, Category, Favorite, Comment, CommentLike
 from .forms import CommentForm
 
@@ -11,23 +12,28 @@ def home(request):
     category_id = request.GET.get('category') # Récupération de l'ID de la catégorie depuis les paramètres de la requête
 
     # On recupère tous les articles, triés par date de création décroissante, et on précharge les sources associées pour éviter les requêtes supplémentaires
-    articles = Article.objects.all().order_by('-created_at').prefetch_related('sources')
+    article_liste = Article.objects.all().order_by('-created_at').prefetch_related('sources')
 
     recent_comments = Comment.objects.all().order_by('-created_at')[:5]
 
     # Filtrage par recherche si une requête est présente
     if query:
-        articles = articles.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+        article_liste = article_liste.filter(Q(title__icontains=query) | Q(summary__icontains=query))
     
     # Filtrage par catégorie si une catégorie est sélectionnée
     if category_id:
-        articles = articles.filter(category_id=category_id)
+        article_liste = article_liste.filter(category_id=category_id)
+
+    # Logique de pagination
+    paginator = Paginator(article_liste, 10) # 10 article par page
+    page_number = request.GET.get('page') # On recupere le numero de la page dams l'url
+    page_obj = paginator.get_page(page_number)
 
     # Récupération de toutes les catégories pour les afficher dans le menu de filtrage
     categories = Category.objects.all()
 
     content = {
-        'articles': articles,
+        'page_obj' : page_obj,
         'categories': categories,
         'recent_comments' : recent_comments,
     }
