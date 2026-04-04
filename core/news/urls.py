50d5edd63058850c6161f8b46cc8177bfdfc751b
Vlan <23ik069si@esisalama.org>
2026-04-05 00:38:03 +0200

    add favicon

--- core/news/urls.py
+++ core/news/urls.py
@@ -1,3 +1,5 @@
+from django.views.generic.base import RedirectView
+from django.conf import settings
 from django.urls import path
 from . import views
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path('favorites/', views.my_favorites, name='my_favorites'), # URL pour afficher les favoris de l'utilisateur
     path('profile/', views.profile, name='profile'),
     path('article/<int:pk>/', views.article_detail, name='article_detail'),
-    path('comment/like/<int:comment_id>/', views.like_comment, name='like_comment')
+    path('comment/like/<int:comment_id>/', views.like_comment, name='like_comment'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.png')),
 ]
