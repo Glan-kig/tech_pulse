6354d00f9b3623b488e3aa30025ec9e929b302e0
Vlan <23ik069si@esisalama.org>
2026-06-29 00:37:58 +0200

    Fix: Ajout du fichier de validation Search Console au bon dossier

--- core/news/urls.py
+++ core/news/urls.py
@@ -1,6 +1,7 @@
 from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -15,4 +16,11 @@ urlpatterns = [
     path('comment/like/<int:comment_id>/', views.like_comment, name='like_comment'),
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.png')),
+    path(
+        "googlee536a5e64b4fa2b6.html",
+        TemplateView.as_view(
+            template_name="googlee536a5e64b4fa2b6.html",
+            content_type="text/html",
+        ),
+    ),
 ]
