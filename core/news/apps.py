ab7d643ffd1dc59ca80ca69b116c14ce6c821fd2
Vlan <23ik069si@esisalama.org>
2026-06-30 16:04:41 +0200

    fix: make welcome email asynchronous with threading

--- core/news/apps.py
+++ core/news/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.news'
+    name = 'news'
 
     def ready(self):  
-        import core.news.signals
+        import news.signals
